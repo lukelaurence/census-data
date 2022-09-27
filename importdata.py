6c9cbf9967bd663df31175a8190e3893eb3e7c8b
lukelaurence <48737821+lukelaurence@users.noreply.github.com>
2022-09-27 18:20:25 -0500

    less storage-heavy

--- importdata.py
+++ importdata.py
@@ -1,7 +1,9 @@
-def condensecensusdata():
+import sys,smart_open
+
+def cleanfile(path):
 	ILLINOIS = '21'
 	COOK = '310'
-	with open('census.csv','r') as f:
+	with smart_open.open(path,encoding='utf8') as f:
 		header = next(f)
 		print(header,end='')
 		h = header.split(',')
@@ -11,6 +13,10 @@ def condensecensusdata():
 			y = x.split(',')
 			if y[state] == ILLINOIS and y[county] == COOK:
 				print(x,end='')
+
+with open('cookcountycensus.csv','w') as f1:
+	sys.stdout = f1
+	cleanfile('census_microdata.csv.gz')
 
 with open('cookcountycensus.csv','r') as f:
 	a = 2000
