cd618217b8d92600d003d767fd8a146698358688
lukelaurence <48737821+lukelaurence@users.noreply.github.com>
2022-09-30 02:45:46 -0500

    update

--- importdata.py
+++ importdata.py
@@ -1,27 +1,62 @@
-import sys,smart_open
+import smart_open,gzip,PyPDF2,re
 
 def cleanfile(path):
 	ILLINOIS = '21'
 	COOK = '310'
 	with smart_open.open(path,encoding='utf8') as f:
 		header = next(f)
-		print(header,end='')
+		yield header
 		h = header.split(',')
-		state = h.index('"STATEICP"')
-		county = h.index('"COUNTYICP"')
+		STATE = h.index('"STATEICP"')
+		COUNTY = h.index('"COUNTYICP"')
 		for x in f:
 			y = x.split(',')
-			if y[state] == ILLINOIS and y[county] == COOK:
-				print(x,end='')
+			if y[STATE] == ILLINOIS and y[COUNTY] == COOK:
+				yield x
 
-with open('cookcountycensus.csv','w') as f1:
-	sys.stdout = f1
-	cleanfile('census_microdata.csv.gz')
+def cleandata():
+	with gzip.open('cookcountycensus.csv.gz','wb') as f1:
+		for x in cleanfile('census_microdata.csv.gz'):
+			f1.write(x.encode())
 
-with open('cookcountycensus.csv','r') as f:
-	a = 2000
+def iscode(input):
+	if len(input) != 8:
+		return False
+	for index,digit in enumerate(input):
+		if index == 2:
+			if digit != " ":
+				return False
+		else:
+			if digit not in "0123456789":
+				return False
+	return True
+
+def getpumas(year):
+	with open(f'{year}_PUMA_Names.pdf','rb') as f:
+		with open(f'{year}_PUMA_Names.txt','w') as f2:
+			reader = PyPDF2.PdfFileReader(f)
+			for x in range(reader.numPages):
+				page = re.sub(' +',' ',reader.getPage(x).extractText()).split('\n')
+				for line in page:
+					if not iscode(line[:8]):
+						continue
+					statefp,puma,*name = line.split(" ")
+					f2.write(puma+';'+' '.join(name)+'\n')
+
+with smart_open.open('cookcountycensus.csv.gz',encoding='utf8') as f:
+	header = next(f)
+	print(header)
+	h = header.split(',')
+	YEAR = h.index('"YEAR"')
+	years = {}
 	for x in f:
-		print(x,end='')
-		a -= 1
-		if not a:
-			exit(0)
+		y = x.split(',')
+		year = y[YEAR]
+		if year not in years:
+			years[year] = 0
+			print(x,end='')
+		elif years[year] < 5:
+			years[year] += 1
+			print(x,end='')
+		else:
+			continue
