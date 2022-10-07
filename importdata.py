707746680db022641f7a4bd909cbb0c892e2a2cb
lukelaurence <48737821+lukelaurence@users.noreply.github.com>
2022-10-07 14:34:13 -0500

    Added function descriptors for inheritance

--- importdata.py
+++ importdata.py
@@ -1,6 +1,16 @@
 import smart_open,gzip,PyPDF2,re
 
 def cleanfile(path='census_microdata.csv.gz'):
+	"""
+	Takes a gzip census file and returns a generator yielding only the rows
+	that are in Cook County and have a valid ownership value.
+	
+	Requires the microdata to have the parameters STATEICP, COUNTYICP,
+	and OWNERSHIP
+
+	inputs:
+	path: path to the file
+	"""
 	ILLINOIS = '21'
 	COOK = '310'
 	with smart_open.open(path,encoding='utf8') as f:
@@ -16,11 +26,28 @@ def cleanfile(path='census_microdata.csv.gz'):
 				yield x
 
 def printdata(input,output,function):
+	"""
+	Given an operation that works on a gzip file (such as cleandata
+	or getgeorange), applies that function and streams the output to another
+	gzip file
+
+	inputs:
+	input: path to gzip file (minus suffix) that is being transformed
+	output: file to write to (also minus suffix)
+	function: function to be applied to input
+	"""
 	with gzip.open(f'{output}.csv.gz','wb') as f1:
 		for x in function(f'{input}.csv.gz'):
 			f1.write(x.encode())
 
 def iscode(input):
+	"""
+	Determines whether an item from the list of PUMAs is a valid PUMA,
+	as opposed to a header
+
+	input:
+	input: string
+	"""
 	if len(input) != 8:
 		return False
 	for index,digit in enumerate(input):
@@ -33,6 +60,10 @@ def iscode(input):
 	return True
 
 def getpumas(year):
+	"""
+	Transforms a list of PUMA names (which are available in PDF) into text and
+	creates a text file containing the valid PUMAs
+	"""
 	with open(f'{year}_PUMA_Names.pdf','rb') as f:
 		with open(f'{year}_PUMA_Names.txt','w') as f2:
 			reader = PyPDF2.PdfFileReader(f)
@@ -45,6 +76,10 @@ def getpumas(year):
 					f2.write(puma+';'+' '.join(name)+'\n')
 
 def getsample():
+	"""
+	Sampling script for testing. Prints first five members of each year
+	of Cook County census data
+	"""
 	with smart_open.open('cookcountycensus.csv.gz',encoding='utf8') as f:
 		header = next(f)
 		print(header)
@@ -64,6 +99,14 @@ def getsample():
 				continue
 
 def getacceptable(input):
+	"""
+	Given text files of acceptable wards or PUMAs by year, which are preceded
+	by the year, returns a dictionary mapping years to the acceptable
+	values within that year
+
+	inputs:
+	input: name of text file (no suffix)
+	"""
 	output = {}
 	with open(f'{input}.txt','r') as f:
 		for x in f:
@@ -71,7 +114,14 @@ def getacceptable(input):
 			output[head] = tail
 	return output
 
-def getsouthside(path='cookcountycensus.csv.gz'):
+def getgeorange(path='cookcountycensus.csv.gz'):
+	"""
+	Given a gzipped csv of census data from Cook County, only yields data
+	within acceptable wards or PUMAs. If a year has neither, yields everything.
+
+	input:
+	path: path to input file
+	"""
 	wards = getacceptable("acceptablewards")
 	pumas = getacceptable("acceptablepumas")
 	with smart_open.open(path,encoding='utf8') as f:
@@ -91,10 +141,18 @@ def getsouthside(path='cookcountycensus.csv.gz'):
 				if '0'+y[PUMA] in pumas[year] or y[PUMA] in pumas[year]:
 					yield x
 			else:
+				# get rid of this branch to only output whitelisted data
 				yield x
 
-def analyzedata():
-	with smart_open.open('southside.csv.gz',encoding='utf8') as f:
+def analyzedata(path='southside.csv.gz'):
+	"""
+	Given a gzipped csv of data within the acceptable geographic range,
+	aggregates ownership data by race
+
+	inputs:
+	path: path to input file
+	"""
+	with smart_open.open(path,encoding='utf8') as f:
 		h = next(f).split(',')
 		YEAR = h.index('"YEAR"')
 		OWNERSHIP = h.index('"OWNERSHP"')
@@ -116,9 +174,14 @@ def analyzedata():
 		return output
 
 def assortdata():
+	"""
+	Outputs comma-deliniated text grouping ownership rates by white and nonwhite
+	race. Can provide full breakdown by race with minor changes. Due to how the
+	function accesses local variables from the stack, declaring any between
+	"whites" and "three_plus" while testing could break it
+	"""
 	header = True
-	# print('year','ownership rate','percent white',sep=',')
-	for year,items in analyzedata().items():
+	for year,items in analyzedata('southside.csv.gz').items():
 		for x in range(1,10):
 			if str(x) not in items[1].keys():
 				items[1][str(x)] = 0
@@ -139,11 +202,11 @@ def assortdata():
 		notwhite = sum(list(l.values())[racestart+1:raceend])
 		ownership_rate = owners/(owners+renters)
 		percent_white = whites/(whites+notwhite)
-		# print(year,ownership_rate,percent_white,sep=',')
 		if header:
 			print('year','ownership rate',*list(l.keys())[racestart:raceend],sep=',')
 			header = False
 		print(year,ownership_rate,*list(l.values())[racestart:raceend],sep=',')
 
-# printdata('cookcountycensus','southside',getsouthside)
-assortdata()
+def main():
+	# printdata('cookcountycensus','southside',getgeorange)
+	assortdata()
